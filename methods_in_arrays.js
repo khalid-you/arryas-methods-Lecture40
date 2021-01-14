5b5ee1f90d61c1693d84d5725694477b5842087a
khalid <khalidyoy240@gmail.com>
2021-01-14 05:43:37 -0800

    nothing new

--- methods_in_arrays.js
+++ methods_in_arrays.js
@@ -1,5 +1,5 @@
 'use strict';
-/*
+
 // This is how to add an element at the end
 const friends = ['Khalid', 'Meshal', 'Ahmed'];
 friends.push('peter');
@@ -15,7 +15,7 @@ const poped = friends.pop();  // Remove the last element
 console.log(poped);
 console.log(friends);
 
-friends.shift();    // Remove the first emelment
+friends.shift();    // Remove The First Emelment
 console.log(friends);
 
 // Check if the element is in the array and return the number of it
@@ -23,6 +23,5 @@ const check = prompt('enter a name to check');
 if (friends.includes(check)) {
     console.log(`${check} is one of your friend and the index of it is ${friends.indexOf(check)}`);
 } else {
-    console.log(`There's no person with ${check} name`);
+    console.log(`WHo's ${check} ??`);
 }
-*/
